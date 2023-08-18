1784a815958714ac81ab4ecf823cf09d5e281c6d
noah WI <noah.windelhed@elev.ga.ntig.se>
2023-08-18 11:28:01 +0200

    test

--- Solsystemet.py
+++ Solsystemet.py
@@ -1,2 +1,13 @@
-jorden = {'mass': 5.972 * 10**24, 'radie' : 6371 * 1000, 'g' : 9.82}
-moon = {'mass' : 7.35 * 10**24, 'radie': 1737.4 * 1000, 'g moon': 1.62}
+jorden = {'mass': 5.972 * 10**24, 'radie': 6371 * 1000, 'g': 9.82}
+moon = {'mass': 7.35 * 10**24, 'radie': 1737.4 * 1000, 'g moon': 1.62}
+
+
+class Planet:
+    def __init__(self, mass, radius, g):
+        self.mass = mass
+        self.radius = radius
+        self.g = g
+
+
+moon = Planet(7.35 * 10**24, 1737.4 * 1000, 1.62)
+earth = Planet(5.972 * 10**24, 6371 * 1000, 9.82)
