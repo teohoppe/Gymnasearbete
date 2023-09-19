1cf264699ab102ee5b6ee45695627f520c2e7bf3
noah WI <noah.windelhed@elev.ga.ntig.se>
2023-09-19 14:41:19 +0200

    bränsle uträkcning

--- Solsystemet.py
+++ Solsystemet.py
@@ -15,24 +15,39 @@ orbit_and_delta_v = {"LEO": 9700, "TLI": 3150, "MCC": 500,
 
 total_delta_v = sum(orbit_and_delta_v.values())
 
+class Engine:
+    def __init__(self, weight, thrust, fuel_com_per_sec) -> None:
+        self.weight = weight
+        self.thrust = thrust
+        self.fuel_com_per_sec = fuel_com_per_sec
+        
+    
 
 class Rocket:
-    def __init__(self, speed, fuel, mass, thrust):
+    def __init__(self, speed, fuel, mass, engine):
         self.speed = speed
         self.fuel = fuel
         self.mass = mass
-        self.thrust = thrust
+        self.engine = engine 
 
     def fuel_mass(self):
         fuel_mass = 1
         return self.fuel * fuel_mass
 
     def delta_v(self):
-        delta_V = self.thrust * math.log((self.mass + self.fuel_mass()) / (self.mass))
+        delta_V = self.engine.thrust * math.log((self.mass + self.fuel_mass()) / self.mass)
         return delta_V
+    
+    def test(self):
+        return self.engine.thrust
 
+RS_25_Engine = Engine(2390, 232375*10, 999)
 
-rocket = Rocket(10, 10, 12, 77700)
+# https://www.spaceflightinsider.com/hangar/falcon-9/
+SpaceX_Merlin = Engine(470, 981*10, 140)
 
+rocket = Rocket(10, 10, 12, SpaceX_Merlin)
+
+print(rocket.test())
 
 print(rocket.delta_v())
