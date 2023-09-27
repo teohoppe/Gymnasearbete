a0f61ef8dcc0fd6fababf3cfffacd3e44e145eb1
noah WI <noah.windelhed@elev.ga.ntig.se>
2023-09-27 21:25:35 +0200

    push för det gick bra

--- Solsystemet.py
+++ Solsystemet.py
@@ -14,48 +14,44 @@ orbit_and_delta_v = {"LEO": 9700, "TLI": 3150, "MCC": 500,
                      "LOI": 350, "GTO": 3200, "LO": 700, "LS": 1600}
 
 total_delta_v = sum(orbit_and_delta_v.values())
+
 print(f"Delta V needed: {total_delta_v}")
 
 class Engine:
     def __init__(self, weight, exhaust_v) -> None:
         self.weight = weight
         self.exhaust_v = exhaust_v
-        
+
 class Rocket:
-    def __init__(self, speed, fuel, mass, engine):
+    def __init__(self, speed, fuel, mass, engine, delta_v):
         self.speed = speed
         self.fuel = fuel
         self.mass = mass
-        self.engine = engine 
+        self.engine = engine
+        self.delta_v = delta_v
 
-    def fuel_mass(self):
-        fuel_mass = 1
-        return self.fuel * fuel_mass
-
-    def delta_v(self):
+    def calc_fuel_needed_and_delta_v(self):
         self.mass += self.engine.weight
-        delta_V = self.engine.exhaust_v * math.log((self.mass + self.fuel) / self.mass)
-        return delta_V
-    
-    def test(self):
-        return self.engine.exhaust_v
-    
-    def calc_fuel_needed(self):
-        """self.mass += self.engine.weight
-        print(self.mass)
-        delta_v = int(input("How much delta V do you want:  "))
-        self.fuel = ((math.e**(delta_v/self.engine.exhaust_v)) / self.mass) - self.mass
-        print(math.e**(delta_v/self.engine.exhaust_v) / self.mass)"""
+        
+        # delta v input
+        delta_v_input = int(input("How much delta V do you want:  "))
+        
+        # Clalc fuel needed
+        self.fuel = ((math.e ** ((delta_v_input/self.engine.exhaust_v))) * self.mass) - self.mass 
+        
+        # clalc delta v
+        self.delta_V = self.engine.exhaust_v * math.log((self.mass + self.fuel) / self.mass)
+        
+        print(f"fuel needed: {self.fuel} kg")
+        print(f"delta V given {self.delta_V} m/s")
+        
+        return self.delta_V, self.fuel
 
 RS_25_Engine = Engine(2390, 232375*10)
 
 # https://www.spaceflightinsider.com/hangar/falcon-9/
 SpaceX_Merlin = Engine(470, 3050)
 
-rocket = Rocket(10, 2363734.024, 3900, SpaceX_Merlin)
+rocket = Rocket(10, 0, 3900, SpaceX_Merlin, 0)
 
-rocket_fuel_mass_needed = 349487
-
-rocket.calc_fuel_needed()
-
-print(rocket.delta_v())
+rocket.calc_fuel_needed_and_delta_v()
--- test_raket.py
+++ test_raket.py
@@ -1,8 +1,14 @@
-engine_thrust = 3050
-mass = 3900
-fuel = 93
 import math
 
+engine_e_x  = 3050
+mass = 3900
 
-delta_V = engine_thrust * math.log((mass + fuel) / mass)
+delta_V = int(input(""))
+
+fuel = 2363734.1476801927
+# Calculate delta-v
+delta_V = engine_e_x * math.log((mass + fuel) / mass)
+
+# Calculate fuel needed
+print(fuel)
 print(delta_V)
