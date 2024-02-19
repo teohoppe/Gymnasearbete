5b421d246fa250a4ba36f8497ba77bc7d8c96b52
noah WI <noah.windelhed@elev.ga.ntig.se>
2024-02-19 08:22:21 +0100

    Delete unnecessary test files and unused code

--- Solsystemet.py
+++ Solsystemet.py
@@ -1,35 +1,43 @@
 import math
-# Karta för delta_v för att åka i
-# https://web.archive.org/web/20070701211813/http://www.pma.caltech.edu/~chirata/deltav.html
 
-# Trans lunar ijection = TLI
-# Mid Course Correction = MCC
-# Lunar Orbit Insertion => enter lunar orbit = LOI
-# Transfer Orbit = GTO
-# Lunar Orbit = LO
-# Lunar Surface = LS
-# Dubbelkolla GTO
-# Delta_V to LOE to LO = 3900
+# Map of delta_v values for space travel, source: https://web.archive.org/web/20070701211813/http://www.pma.caltech.edu/~chirata/deltav.html
 
+# Abbreviations for different stages of lunar mission
+# TLI: Trans Lunar Injection
+# MCC: Mid Course Correction
+# LOI: Lunar Orbit Insertion (enter lunar orbit)
+# GTO: Geostationary Transfer Orbit
+# LO: Lunar Orbit
+# LS: Lunar Surface
+# Note: Delta_V from Low Earth Orbit (LEO) to Lunar Orbit (LO) = 3900
+
+# Dictionary mapping orbits to their respective delta_v values in m/s
 orbit_and_delta_v_moon = {"LEO": 9700, "TLI": 3150, "MCC": 500,
-                     "LOI": 350, "GTO": 3200, "LO": 700, "LS": 1600}
+                          "LOI": 350, "GTO": 3200, "LO": 700, "LS": 1600}
 
+# Calculate total delta_v for all stages
 total_delta_v = sum(orbit_and_delta_v_moon.values())
 
-# Delta_V to LOE to LO = 3900 + MCC
+# Total delta_v from Low Earth Orbit to Lunar Orbit including MCC
 total_delta_v_from_orbit = 3200 + 700 + 1600
 
 print(f"Delta V needed from LEO: {total_delta_v_from_orbit}")
 
+
 class Engine:
-    def __init__(self, weight, exhaust_v, ISP, flow_rate, trusth) -> None:
+    """Represents a rocket engine with various parameters."""
+
+    def __init__(self, weight, exhaust_v, ISP, flow_rate, thrust) -> None:
         self.weight = weight
         self.exhaust_v = exhaust_v
         self.ISP = ISP
         self.flow_rate = flow_rate
-        self.trusth = trusth
+        self.thrust = thrust
+
 
 class Rocket:
+    """Represents a rocket with attributes like speed, fuel, mass, and engine."""
+
     def __init__(self, speed, fuel, mass, engine, delta_v, sum_exhaust_v):
         self.speed = speed
         self.fuel = fuel
@@ -39,82 +47,84 @@ class Rocket:
         self.sum_exhaust_v = sum_exhaust_v
 
     def calc_fuel_and_delta_v(self):
+        """Calculates the fuel needed and the resulting delta_v based on engine efficiency."""
         self.mass += self.engine.weight
-        
-        # delta v input
+
+        # User input for desired delta_v
         delta_v_input = int(input("How much delta V do you want:  "))
 
-        # Clalc fuel needed
-        self.fuel = math.ceil(((math.e ** ((delta_v_input/self.engine.exhaust_v))) * self.mass) - self.mass)
+        # Calculate fuel needed using the rocket equation
+        self.fuel = math.ceil(
+            (math.exp(delta_v_input / self.engine.exhaust_v) * self.mass) - self.mass)
 
-        # clalc delta v
-        self.delta_V = self.engine.exhaust_v * \
+        # Calculate achievable delta_v with the added fuel
+        self.delta_v = self.engine.exhaust_v * \
             math.log((self.mass + self.fuel) / self.mass)
 
-        print(f"fuel needed: {self.fuel} kg")
-        print(f"delta V given {round(self.delta_V)} m/s")
+        print(f"Fuel needed: {self.fuel} kg")
+        print(f"Delta V achieved: {round(self.delta_v)} m/s")
 
-        return self.delta_V, self.fuel
+        return self.delta_v, self.fuel
 
     def fuel_expense(self):
-        
+        """Calculates the cost of fuel based on engine type and fuel requirements."""
         if self.engine == SpaceX_Merlin:
             sum_ratio = 2.27 + 1
             part_of_fuel = self.fuel / sum_ratio
 
             LOX_fuel = part_of_fuel * 2.27
-            RP_1_fuel = part_of_fuel * 1
+            RP_1_fuel = part_of_fuel
 
             LOX_fuel_price = LOX_fuel * 0.27
             RP_1_fuel_price = RP_1_fuel * 2.3
 
-            total_fuel_price_in_dollars = round(LOX_fuel_price + RP_1_fuel_price)
+            total_fuel_price_in_dollars = round(
+                LOX_fuel_price + RP_1_fuel_price)
 
-            print(f"Totala pris: {total_fuel_price_in_dollars}")
-        
+            print(f"Total cost: {total_fuel_price_in_dollars}")
+
         elif self.engine == Spacex_Raptor:
             sum_ratio_raptor = 3.6 + 1
             part_of_fuel_raptor = self.fuel / sum_ratio_raptor
 
             LOX_fuel_raptor = part_of_fuel_raptor * 3.6
-            ch4_fuel_raptor = part_of_fuel_raptor * 1
+            CH4_fuel_raptor = part_of_fuel_raptor
 
-            LOX_fuel_price_raptor = LOX_fuel * 0.27
-            ch4_fuel_price_raptor = ch4_fuel_raptor * 8.8
-            
-            total_fuel_price_in_dollars_raptor = round(LOX_fuel_price_raptor + ch4_fuel_price_raptor)
-            print(f"Totala pris: {total_fuel_price_in_dollars_raptor}")
-            
+            LOX_fuel_price_raptor = LOX_fuel_raptor * 0.27
+            CH4_fuel_price_raptor = CH4_fuel_raptor * 8.8
+
+            total_fuel_price_in_dollars_raptor = round(
+                LOX_fuel_price_raptor + CH4_fuel_price_raptor)
+            print(f"Total cost: {total_fuel_price_in_dollars_raptor}")
+
         elif self.engine == rs68:
             sum_ratio_rs68 = 5.97 + 1
             part_of_fuel_rs68 = self.fuel / sum_ratio_rs68
 
             LOX_fuel_rs68 = part_of_fuel_rs68 * 5.97
-            LH2_fuel_rs68 = part_of_fuel_rs68 * 1
+            LH2_fuel_rs68 = part_of_fuel_rs68
 
             LOX_fuel_price_rs68 = LOX_fuel_rs68 * 0.27
-            LH2_fuel_price_rs68 = LH2_fuel_rs68 * 6.1 
+            LH2_fuel_price_rs68 = LH2_fuel_rs68 * 6.1
 
-            total_fuel_price_in_dollars_rs68 = round(LOX_fuel_price_rs68 + LH2_fuel_price_rs68)
-            print(f"Totala pris: {total_fuel_price_in_dollars_rs68}")
-        
-        # https://spaceimpulse.com/2023/06/13/how-much-does-rocket-fuel-cost/
-        # RP-1 presents a cheaper option at $2.3/kg.
-        # while LOX comes in at $0.27/kg
-        # https://link.springer.com/chapter/10.1007/978-0-387-09630-8_4 STATES RATIO (2.27:1 mixture ratio of LOX to RP-1)
+            total_fuel_price_in_dollars_rs68 = round(
+                LOX_fuel_price_rs68 + LH2_fuel_price_rs68)
+            print(f"Total cost: {total_fuel_price_in_dollars_rs68}")
 
 
-# https://www.spaceflightinsider.com/hangar/falcon-9/
-# SpaceX_Merlin flowrate is 140 kg/s, to calc change in exhous velocity
+# Engine instances with their specifications
 SpaceX_Merlin = Engine(470, 3050, 342, 140, 981000)
-Spacex_Raptor = Engine(1600 , 3236, 350, 650, 2230000)   #https://forum.nasaspaceflight.com/index.php?topic=47506.1520, https://en.wikipedia.org/wiki/SpaceX_Raptor 
-rs68 = Engine(6600, 4464, 412, 550, 3370000) # https://en.wikipedia.org/wiki/RS-68, https://www.slideshare.net/JackTaylor20/rs68 
+Spacex_Raptor = Engine(1600, 3236, 350, 650, 2230000)
+rs68 = Engine(6600, 4464, 412, 550, 3370000)
 
+# Rocket instances with initial parameters
 rocket_rapt = Rocket(10, 0, 4000, Spacex_Raptor, 0, 0)
 rocket_merl = Rocket(10, 0, 3900, SpaceX_Merlin, 0, 0)
 rocket_rs = Rocket(10, 0, 3900, rs68, 0, 0)
 
+
 def what_rocket_do_you_want():
+    """Prompts the user to select a rocket engine and returns the corresponding rocket object."""
     print("Select an engine:\n1. SpaceX Merlin\n2. SpaceX Raptor\n3. RS-68")
     choice = input("Enter the number of your choice: ")
     if choice == '1':
@@ -126,6 +136,7 @@ def what_rocket_do_you_want():
     else:
         print("Invalid choice. Defaulting to SpaceX Merlin.")
         return rocket_merl
+
 
 rocket = what_rocket_do_you_want()
 
--- test_raket.py
+++
@@ -1,8 +0,0 @@
-import math
-
-# write a code that uses the rocket equation to determen how much fuel you need to reach a certain height
-# the rocket equation is: F = m * a
-
-
-
-
